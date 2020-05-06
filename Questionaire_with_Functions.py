d8b04b5ba3e8db3491cd84715c633e8f5de732cc
LilitSayian <61308445+LilitSayian@users.noreply.github.com>
2020-05-06 23:37:40 +0400

    Add files via upload

--- Questionaire_with_Functions.py
+++ Questionaire_with_Functions.py
@@ -1,27 +1,46 @@
-def main():
-
-        Question1=input("Does your child have difficulty talking? (yes/ no) ").lower()
-        if Question1 == "yes" :
+def answer_question1():
+    while True:
+        Question1 = input("Does your child have difficulty talking? (yes/ no) ").lower()
+        if Question1 == "yes":
             print('Register for a speech therapy session with the logopedic')
+            break
         else:
-             if Question1 == "no" :
-                 print('Do not register for a speech therapy session with the logopedic')
+            if Question1 == "no":
+                print('Do not register for a speech therapy session with the logopedic')
+                break
 
+
+def answer_question2():
+    while True:
         Question2 = input("Does your child have difficulty walking? (yes/ no) ").lower()
         if Question2 == "yes":
             print('Register for a physiotherapy session')
+            break
         else:
             if Question2 == "no":
                 print('Do not register for a physiotherapy session')
+                break
 
-        Question3=input("Does your child have developmental challenges? (yes/ no) ").lower()
-        if Question3 == "yes" :
+
+def answer_question3():
+    while True:
+        Question3 = input("Does your child have developmental challenges? (yes/ no) ").lower()
+        if Question3 == "yes":
             print('Register for a child psychology session')
+            break
         else:
             if Question3 == "no":
                 print('Do not register for a child psychology session')
+                break
 
-        print('Thank you. Now your appointments can be done.')
+
+def main():
+
+    answer_question1()
+    answer_question2()
+    answer_question3()
+
+    print('Thank you. Now your appointments can be done.')
 
 
 main()
