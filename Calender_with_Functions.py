98eb9d110145d66ae7b80503ca25aa5e97454673
LilitSayian <61308445+LilitSayian@users.noreply.github.com>
2020-05-06 15:08:51 +0400

    Add files via upload

--- Calender_with_Functions.py
+++ Calender_with_Functions.py
@@ -1,41 +1,57 @@
 import calendar
 
-def decide_Day(possible_days, used_days, available_days) :
+
+def decide_Day(possible_days, used_days, available_days):
     for item in possible_days.keys():
         if item not in used_days:
             available_days.append(item)
+
 
 def chooseDay(used_days, available_days):
     while True:
         user_day = input("Please choose a suitable day for you, Saturday or Sunday").lower()
         if (user_day in used_days):
             print("Sorry that day is not available at this moment")
-        else:
-            if (user_day in available_days):
-                print("The day is confirmed.")
-                break
 
-def decide_Time(possible_options,used_options,available_options) :
+        elif (user_day in available_days):
+            print("The day is confirmed.")
+            break
+        else:
+            if ValueError:
+                print("Your value is in valid")
+
+def decide_Time(possible_options, used_options, available_options):
     for item in possible_options.keys():
         if item not in used_options:
             available_options.append(item)
 
-def chooseTime(used_options,available_options,possible_options,not_available_options):
+
+def chooseTime(used_options, available_options, possible_options, not_available_options):
     while True:
-        user_time = int(input("Please choose a suitable time for you, each appointment takes an hour."))
-        if (user_time in used_options):
-            print("Sorry the time is taken")
-        if (user_time in available_options):
-            print(possible_options[str(user_time)])
-            used_options.append(user_time)
-            break
-        if (user_time in not_available_options):
-            print(possible_options[str(user_time)])
+        try:
+            user_time = int(input("Please choose a suitable time for you, each appointment takes an hour."))
+            if (user_time in used_options):
+                print("Sorry the time is taken")
+
+            elif (user_time in available_options):
+                print(possible_options[str(user_time)])
+                used_options.append(user_time)
+                break
+            elif (user_time in not_available_options):
+                print(possible_options[str(user_time)])
+        except:
+            print("That's not a valid option!")
 
 def main():
-
     year = 2020
-    month = int(input("Enter month in numbers: "))
+
+    while True:
+        try:
+            month = int(input("Enter month in numbers: "))
+            break
+        except:
+            print("That's not a valid option!")
+    # month = int(input("Enter month in numbers: "))
     print(calendar.month(year, month))
 
     print("The Logopedic only works on Sundays and Saturdays from 10-4")
@@ -44,16 +60,16 @@ def main():
     used_options = [3, 2]
     available_options = [10, 11, 12]
     used_days = ['sunday']
-    available_days = ['saturday','sunday']
+    available_days = ['saturday', 'sunday']
 
     possible_days = {
-        "Saturday" : "Thank you. Your appointment is made",
-        "Sunday" : "Thank you. Your appointment is made",
+        "Saturday": "Thank you. Your appointment is made",
+        "Sunday": "Thank you. Your appointment is made",
     }
 
     decide_Day(possible_days, used_days, available_days)
     chooseDay(used_days, available_days)
-    
+
     possible_options = {
         "10": "Thank you. Your appointment is made.",
         "11": "Thank you. Your appointment is made.",
@@ -64,8 +80,8 @@ def main():
         "4": "Sorry, by that time the appointments would have ended. Try another suitable time."
     }
 
-    decide_Time(possible_options,used_options,available_options)
-    chooseTime(used_options,available_options,possible_options,not_available_options)
+    decide_Time(possible_options, used_options, available_options)
+    chooseTime(used_options, available_options, possible_options, not_available_options)
 
 
 main()
