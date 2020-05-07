70ee7746dd62ea35e7ac6c16309517e6d145dd0e
LilitSayian <61308445+LilitSayian@users.noreply.github.com>
2020-05-07 22:30:05 +0400

    Add files via upload

--- User_registration_functions.py
+++ User_registration_functions.py
@@ -1,15 +1,23 @@
+import re
 import json
+import os
 
-def main():
-    name = input(" Please Enter Name: ")
-    surname = input(" Please Enter Surname: ")
-    birthMonth = int(input("Enter your birth month (2 digits):"))
-    birthDay = int(input("Enter the day of your birth:"))
-    birthYear = int(input("Enter the year you were born (4 digits):"))
-    condition = int(input(" Please Enter the Condition from 1-5: "))
 
-    print("Thank you for registering.")
+def data():
+    with open("read_user_info.json") as file_data:
+        info = json.load(file_data)
+        return info
 
+
+def openuserinformation(name, surname, birthMonth, birthDay, birthYear, condition):
+    if not os.path.exists("user_information.json"):
+        with open("user_information.json", "w+") as file:
+            initial_dict = {
+                "users": []
+            }
+            json.dump(initial_dict, file, indent=True)
+    with open("user_information.json", "r") as file:
+        data_json = json.load(file)
     user_information = {'name': name,
                         'surname': surname,
                         'birthMonth': birthMonth,
@@ -17,14 +25,99 @@ def main():
                         'birthYear': birthYear,
                         'condition': condition
                         }
-    json_dump = json.dumps(user_information, indent=2)
+    data_json["user_information"].append(user_information)
+    with open("user_information.json", "w+") as f:
+        json.dump(data_json, f, indent=True)
+    return user_information
 
-    print(json_dump)
-    file = open("user_information.json", 'w+')
-    file.write(json_dump)
-    file.close()
-    with open("user_information.json", "r") as f:
-        f.close()
+
+def name():
+    while True:
+        name = input(" Please Enter Name: ")
+        if not re.match("^[a-z]*$", name):
+            print("Error! Only letters a-z allowed!")
+        else:
+            return name
+
+
+def surname():
+    while True:
+        surname = input(" Please Enter Surname: ")
+        if not re.match("^[a-z]*$", surname):
+            print("Error! Only letters a-z allowed!")
+        else:
+            return surname
+
+
+def birthMonth():
+    while True:
+        try:
+            birthMonth = int(input("Enter your birth month :"))
+            if 0 < birthMonth <= 12:
+                return birthMonth
+        except:
+            print("That's not a valid option!")
+
+
+def birthDay():
+    while True:
+        try:
+            birthday = int(input("Enter your birth day :"))
+            if 0 < birthday <= 31:
+                return birthday
+        except:
+            print("That's not a valid option!")
+
+
+def birthYear():
+    while True:
+        try:
+            birthYear = input("Enter the year you were born (4 digits):")
+            birthYear=int(birthYear)
+            if 1919 < int(birthYear) <= 2020:
+                return birthYear
+        except:
+            print("That's not a valid option!")
+
+
+def condition():
+    while True:
+        try:
+            condition = input(" Please Enter the Condition from 1-5: ")
+            condition= int(condition)
+            if 0 < condition <= 5:
+                return condition
+        except:
+            print("That's not a valid option!")
+
+
+def answer_my_question():
+    while True:
+        Question = input("Do you want to know what health specialist we have? (yes/ no) ").lower()
+        if Question == "yes":
+            print(data())
+            break
+        else:
+            if Question == "no":
+                print("Thank you for registering.")
+                break
+
+
+def main():
+    data()
+    user_name = name()
+    user_surname = surname()
+    birth_month = birthMonth()
+    day = birthDay()
+    year = birthYear()
+    _condition = condition()
+    user_information = openuserinformation(user_name, user_surname, birth_month, day, year, _condition)
+    print(user_information)
+    print("Thank you for registering.")
+    answer_my_question()
+    print("Please answer the upcoming questions to make future appointments. Thank you.")
 
 
 main()
+
+
